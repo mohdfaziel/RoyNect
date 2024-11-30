2ed1305ab6d47d407d1f7fd77dce86357d19e4a6
Mohd Faziel <mohdfazel969@gmail.com>
2024-11-30 14:09:21 +0530

    changed image and out of stock

--- src/Main/Buy.jsx
+++ src/Main/Buy.jsx
@@ -6,7 +6,7 @@ export default function Buy() {
     <div>
       <div className="buy  transition-all flex justify-center items-center gap-10">
         <Count/>
-        <button className="bg-white hover:scale-105 transition-all text-active text-3xl font-bold px-9 py-2 rounded-full">Buy Now</button>
+        <button className="bg-white opacity-40 hover:scale-105 transition-all text-active text-3xl font-bold px-9 py-2 rounded-full">Out of Stock</button>
       </div>
     </div>
   );
