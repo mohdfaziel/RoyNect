3789522f66207131d0daf3aafd6c3fc119fcd720
Mohd Faziel <mohdfazel969@gmail.com>
2024-11-30 12:48:10 +0530

    added email

--- src/About Us/Intro.jsx
+++ src/About Us/Intro.jsx
@@ -45,7 +45,7 @@ export default function Intro() {
         </p>
         <div className="icons flex justify-center items-center md:items-start w-full md:w-fit gap-3">
           <Social icon={ig} link="https://www.instagram.com/dawood__beekeeper?igsh=MTd2YnI1MWpjZDFrMQ=="/>
-          <Social icon={email} />
+          <Social icon={email} link="mailto:khaleek.ul92@gmail.com?subject=Inquiry%20About%20Honey&body=Hello%2C%20Dawood%20Ahmed%20Sheikh%21%20I%E2%80%99m%20interested%20in%20purchasing%20your%20honey."/>
           <Social icon={whatsapp} link="https://wa.me/8492000445?text=Hello,%20Dawood%20Ahmed%20Sheikh!%20I%E2%80%99m%20interested%20in%20purchasing%20your%20honey."/>
         </div>
       </motion.div>
