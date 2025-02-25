52b37d5da7920a0bf851a6129d6633413527d125
Mohd Faziel <mohdfazel969@gmail.com>
2025-02-26 01:56:42 +0530

    fix

--- src/pages/Check Out/Submit/Summary.jsx
+++ src/pages/Check Out/Submit/Summary.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Sumry from "../../../assets/Animations/icons/Sumry";
-import Payment from "../../../assets/Animations/icons/payment";
+import Sumry from "../../../assets/Animations/icons/Sumry.jsx";
+import Payment from "../../../assets/Animations/icons/payment.jsx";
 import Pay from "./Pay";
 import {useSelector} from "react-redux";
 function Summary({orderPlacing,setOrderPlacing}) {
