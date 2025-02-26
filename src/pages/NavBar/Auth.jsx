413bcf1e074e28e7a7843c84986868285d3e1951
Mohd Faziel <mohdfazel969@gmail.com>
2025-02-26 11:56:37 +0530

    fix userLogin drop down

--- src/pages/NavBar/Auth.jsx
+++ src/pages/NavBar/Auth.jsx
@@ -6,10 +6,12 @@ export default function Auth() {
     const user = useSelector((state)=> state.user.userData);
     const status = useSelector((state)=> state.user.status);
     const [drop,setDrop] = useState(false);
-    function setdrop()
+    function setdrop(event)
     {
+        event.stopPropagation();
         setDrop((prev)=> !prev);
     }
+    document.body.onclick = ()=> setDrop(false);
     return (
       <div className="relative">
         <div onClick={setdrop} className="Login md:w-[45px] hover:scale-105 transition-all w-[30px] flex justify-center items-center overflow-hidden rounded-full h-[30px] md:h-[45px] cursor-pointer bg-white">
