aed138cd0c9966883f6da80bf8a94a8d7203bca5
Mohd Faziel <mohdfazel969@gmail.com>
2025-03-02 00:13:53 +0530

    Fix

--- src/pages/Product/BuyPhone.jsx
+++ src/pages/Product/BuyPhone.jsx
@@ -4,18 +4,18 @@ import Count from "./Count";
 export default function BuyPhone({onClick, weight, price,qty,setQty }) {
   return (
     <div className="w-full gap-2 md:gap-3 flex transition-all md:hidden flex-col justify-evenly max-h-[25rem] h-fit p-4 bg-white rounded-t-3xl items-start">
-      <h1 className="title text-xl font-extrabold">
+      <h1 className="title text-2xl font-extrabold">
         100% Pure & Natural Apis Cerana Honey
       </h1>
       <p className="weight text-lg font-bold text-gray-500">{weight}kg</p>
       <div className="price&Count w-full flex justify-between items-center">
         <div className="price flex justify-center items-center">
-          <h1 className="text-xl md:text-3xl font-extrabold">{price}</h1>
-          <img src={rupee} loading="lazy" className="w-6" alt="" />
+          <img src={rupee} loading="lazy" className="w-5" alt="" />
+          <h1 className="text-2xl md:text-3xl font-extrabold">{price}</h1>
         </div>
         <Count qty={qty} setQty={setQty}/>
       </div>
-      <button onClick={onClick} className="bg-main hover:scale-105 transition-all w-full text-active text-xl font-extrabold py-2 rounded-full">Add To Cart</button>
+      <button onClick={onClick} className="bg-main hover:scale-105 transition-all w-full text-active text-2xl font-extrabold py-2 rounded-full">Add To Cart</button>
     </div>
   );
 }
--- src/pages/Product/Count.jsx
+++ src/pages/Product/Count.jsx
@@ -6,18 +6,18 @@ export default function Count({qty,setQty})
         <div className="amount flex justify-center items-center gap-5">
           <img
             src={minus}
-            className="cursor-pointer w-7 md:w-10"
+            className="cursor-pointer w-8 md:w-10"
             loading="lazy"
             onClick={() => {
               if (qty > 1) setQty((prevqty) => prevqty - 1);
             }}
             alt=""
           />
-          <p className="md:text-4xl text-xl font-bold min-w-[2rem] md:min-w-[3rem] text-center ">{qty}</p>
+          <p className="md:text-4xl text-2xl font-bold min-w-[2rem] md:min-w-[3rem] text-center ">{qty}</p>
           <img
             src={plus}
             loading="lazy"
-            className="cursor-pointer w-7 md:w-10"
+            className="cursor-pointer w-8 md:w-10"
             onClick={() => {
               if(qty<10){
                 setQty((prevqty) => prevqty + 1)
