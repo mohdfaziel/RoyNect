b8a809732eea959fa0ddd9280241d8ad66ac1ca0
Mohd Faziel <mohdfazel969@gmail.com>
2025-03-13 01:06:54 +0530

    fix

--- src/pages/Admin/APages/OrderInfo/Item.jsx
+++ src/pages/Admin/APages/OrderInfo/Item.jsx
@@ -5,7 +5,7 @@ function Item({title,value}) {
   return (
     <div className='flex justify-center w-full md:w-fit text-white font-semibold text-sm md:text-lg items-center gap-3 bg-Adark1 px-4 py-2 rounded-full'>
       <div>{title} : </div>
-      <div className='flex justify-center items-center text-gray-500 gap-3'>{value} <img onClick={()=> navigator.clipboard.writeText(value)} className='w-4 hover:scale-110 transition-all' src={cpy}></img></div>
+      <div className='flex justify-center items-center text-gray-500 gap-3'>{value.length>10? value.slice(0,10)+" ...": value} <img onClick={()=> navigator.clipboard.writeText(value)} className='w-4 hover:scale-110 transition-all' src={cpy}></img></div>
     </div>
   )
 }
