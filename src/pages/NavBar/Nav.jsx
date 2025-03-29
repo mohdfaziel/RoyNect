cf9d75912d202ce7378e2824178074d76ae38ace
Mohd Faziel <mohdfazel969@gmail.com>
2025-03-30 01:52:46 +0530

    changed brand name

--- src/pages/NavBar/Nav.jsx
+++ src/pages/NavBar/Nav.jsx
@@ -15,6 +15,7 @@ import Cart from "../../components/Animations/Cart";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 export default function Nav() {
   const dispatch = useDispatch();
+  const Navigate = useNavigate();
   const location = useLocation(); 
   if (location.pathname.startsWith("/admin")) {
     return null;
@@ -23,23 +24,9 @@ export default function Nav() {
   return (
     <div className="Nav w-full z-30 md:px-[3rem] px-[1rem] flex top-5 md:top-3 transition-all justify-center items-center min-h-7 fixed">
       <div className="container py-2 w-[100%] flex justify-between items-center">
-        <div className="Logo  px-6 py-2 rounded-full shadow-md bg-white">
+        <div onClick={()=> Navigate("/")} className="Logo cursor-pointer px-6 py-2 rounded-full shadow-md bg-white">
           <img src={brand} className="w-20 md:w-28" alt="" />
         </div>
-        {/* <div className="Options flex justify-center items-center gap-5">
-          <Item
-            title="Products"
-            link=""
-            iconActive={jarActive}
-            iconUnactive={jarUnActive}
-          />
-          <Item
-            title="About us"
-            link="/about"
-            iconActive={aboutActive}
-            iconUnactive={aboutUnActive}
-          />
-        </div> */}
         <div className="Actions flex bg-white py-2 px-10 shadow-md rounded-full justify-center items-center gap-3">
           <Auth/>
           <div
