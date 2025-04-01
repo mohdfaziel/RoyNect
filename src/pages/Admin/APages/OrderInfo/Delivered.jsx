605f78ace528880dd038527b4a3776566ca7560c
Mohd Faziel <mohdfazel969@gmail.com>
2025-04-02 00:35:17 +0530

    Fix order delivered before shipping and refunded before initiating refund

--- src/pages/Admin/APages/OrderInfo/Delivered.jsx
+++ src/pages/Admin/APages/OrderInfo/Delivered.jsx
@@ -1,47 +1,56 @@
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import databaseService from '../../../../Firebase/Services/database';
-import { orderStatusUpdate } from '../../../../Mailer/EmailSenderUser';
-import { orderUpdated } from '../../../../Mailer/EmailSenderAdmin';
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import databaseService from "../../../../Firebase/Services/database";
+import { orderStatusUpdate } from "../../../../Mailer/EmailSenderUser";
+import { orderUpdated } from "../../../../Mailer/EmailSenderAdmin";
 function Delivered({ setLoader, order }) {
-    const [check, setCheck] = useState(Boolean(order.deliveredDate));
-    async function handleCheck(e) {
-        if (check){
-          toast.error("Can't Undo Action");
-          return;
-        }
-        setLoader(true);
-        try {
-            setCheck(true);
-            const date = new Date().toISOString();
-            await databaseService.updateAttribute(order.id, "deliveredDate", date);
-            await databaseService.updateOrderStatus(order.id, "delivered");
-            const updatedOrderData = await databaseService.getOrder(order.id);
-            orderStatusUpdate(updatedOrderData); // Send order delivered email to user
-            orderUpdated(updatedOrderData); // Send order delivered email to admin
-            setLoader(false);
-            toast.success("Order marked as delivered");
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            toast.success("Refresh the Page to see changes");
-        } catch (err) {
-            console.log(err.message);
-        }
+  const [check, setCheck] = useState(Boolean(order.deliveredDate));
+  async function handleCheck(e) {
+    try {
+      if (check) {
+        toast.error("Can't Undo Action");
+        return;
+      }
+      setLoader(true);
+      const {status} = await databaseService.getOrder(order.id);
+      if (status !== "shipped") {
+        toast.error("Order is not shipped yet");
+        setLoader(false);
+        return;
+      }
+      setCheck(true);
+      const date = new Date().toISOString();
+      await databaseService.updateAttribute(order.id, "deliveredDate", date);
+      await databaseService.updateOrderStatus(order.id, "delivered");
+      const updatedOrderData = await databaseService.getOrder(order.id);
+      orderStatusUpdate(updatedOrderData); // Send order delivered email to user
+      orderUpdated(updatedOrderData); // Send order delivered email to admin
+      setLoader(false);
+      toast.success("Order marked as delivered");
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      toast.success("Refresh the Page to see changes");
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    return (
-        <div className='flex justify-center items-center gap-3  bg-Aunactive text-white px-4 py-2 rounded-full'>
-            <label htmlFor="myCheckbox" className="md:text-lg text-base font-medium cursor-pointer">
-                isDelivered
-            </label>
-            <input
-                type="checkbox"
-                id="myCheckbox"
-                checked={check}
-                onChange={handleCheck}
-                className="w-5 h-5 cursor-pointer"
-            />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center gap-3  bg-Aunactive text-white px-4 py-2 rounded-full">
+      <label
+        htmlFor="myCheckbox"
+        className="md:text-lg text-base font-medium cursor-pointer"
+      >
+        isDelivered
+      </label>
+      <input
+        type="checkbox"
+        id="myCheckbox"
+        checked={check}
+        onChange={handleCheck}
+        className="w-5 h-5 cursor-pointer"
+      />
+    </div>
+  );
 }
 
 export default Delivered;
--- src/pages/Admin/APages/OrderInfo/Refunded.jsx
+++ src/pages/Admin/APages/OrderInfo/Refunded.jsx
@@ -1,46 +1,59 @@
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import databaseService from '../../../../Firebase/Services/database';
-import { paymentStatusUpdate } from '../../../../Mailer/EmailSenderUser';
-import { paymentStatusUpdated } from '../../../../Mailer/EmailSenderAdmin';
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import databaseService from "../../../../Firebase/Services/database";
+import { paymentStatusUpdate } from "../../../../Mailer/EmailSenderUser";
+import { paymentStatusUpdated } from "../../../../Mailer/EmailSenderAdmin";
 function Refunded({ setLoader, order }) {
-    const [check, setCheck] = useState(Boolean(order.refunded));
-    async function handleCheck(e) {
-        if (check){
-          toast.error("Can't Undo Action");
-          return;
-        }
-        setLoader(true);
-        try {
-            setCheck(true);
-            await databaseService.updateAttribute(order.id, "refunded", true);
-            await databaseService.updateAttribute(order.id, "paymentStatus", "Refunded");
-            const updatedOrderData = await databaseService.getOrder(order.id);
-            paymentStatusUpdate(updatedOrderData); // Send payment refunded email to user
-            paymentStatusUpdated(updatedOrderData); // Send payment refunded email to admin
-            setLoader(false);
-            toast.success("Refund Complete For the Order");
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            toast.success("Refresh the Page to see changes");
-        } catch (err) {
-            console.log(err.message);
-        }
+  const [check, setCheck] = useState(Boolean(order.refunded));
+  async function handleCheck(e) {
+    try {
+      if (check) {
+        toast.error("Can't Undo Action");
+        return;
+      }
+      setLoader(true);
+      const { status } = await databaseService.getOrder(order.id);
+      if (status !== "Refund Initiated") {
+        toast.error("Refund is not initiated yet");
+        setLoader(false);
+        return;
+      }
+      setCheck(true);
+      await databaseService.updateAttribute(order.id, "refunded", true);
+      await databaseService.updateAttribute(
+        order.id,
+        "paymentStatus",
+        "Refunded"
+      );
+      const updatedOrderData = await databaseService.getOrder(order.id);
+      paymentStatusUpdate(updatedOrderData); // Send payment refunded email to user
+      paymentStatusUpdated(updatedOrderData); // Send payment refunded email to admin
+      setLoader(false);
+      toast.success("Refund Complete For the Order");
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      toast.success("Refresh the Page to see changes");
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    return (
-        <div className='flex justify-center items-center gap-3 bg-Aunactive text-white px-4 py-2 rounded-full'>
-            <label htmlFor="myCheckbox" className="md:text-lg text-base font-medium cursor-pointer">
-                Order Refunded
-            </label>
-            <input
-                type="checkbox"
-                id="myCheckbox"
-                checked={check}
-                onChange={handleCheck}
-                className="w-5 h-5 cursor-pointer"
-            />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center gap-3 bg-Aunactive text-white px-4 py-2 rounded-full">
+      <label
+        htmlFor="myCheckbox"
+        className="md:text-lg text-base font-medium cursor-pointer"
+      >
+        Order Refunded
+      </label>
+      <input
+        type="checkbox"
+        id="myCheckbox"
+        checked={check}
+        onChange={handleCheck}
+        className="w-5 h-5 cursor-pointer"
+      />
+    </div>
+  );
 }
 
 export default Refunded;
