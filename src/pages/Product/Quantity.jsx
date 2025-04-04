2d9f4e2ecdc3d71a553d0804e1e874934cb63d46
Mohd Faziel <mohdfazel969@gmail.com>
2025-04-05 00:31:39 +0530

    Added cursor pointer to the quantity changing buttons

--- src/pages/Product/Quantity.jsx
+++ src/pages/Product/Quantity.jsx
@@ -11,7 +11,7 @@ export default function Quantity({bg,txt,weight,onClick}) {
         unactive: "text-unactive",
     }[txt];
   return (
-    <div onClick={onClick} className={classNames(bgClass,"w-[45px] hover:scale-105 transition-all p-1 flex justify-center items-center h-[45px] rounded-full")}>
+    <div onClick={onClick} className={classNames(bgClass,"w-[45px] cursor-pointer hover:scale-105 transition-all p-1 flex justify-center items-center h-[45px] rounded-full")}>
       <p className={classNames(textClass,"text-sm font-semibold")}>{weight}</p>
     </div>
   )
