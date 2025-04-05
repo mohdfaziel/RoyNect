0b6a578da327b5cb56d305da712d9b9949943d04
Mohd Faziel <mohdfazel969@gmail.com>
2025-04-05 13:38:51 +0530

    Modified shippingCost function

--- src/ShippingDetails/shippingCost.js
+++ src/ShippingDetails/shippingCost.js
@@ -1,13 +1,21 @@
-import { useSelector } from "react-redux";
-const shippingCost = async (dest_pin,weight) => {
+import axios from "axios";
+const shippingCost = async (dest_pin, weight) => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/shipping-cost?origin_pincode=${182222}&dest_pincode=${dest_pin}&weight=${weight}`);
-        const data = await response.json(); 
-        return data[0]; 
-    } catch (err) {
-        console.error("Error fetching shipping cost:", err.message);
-        return null; 
-    }
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/shipping-cost`,
+          {
+            params: {
+              dest_pincode: dest_pin,
+              weight: weight,
+            },
+          }
+        );
+    
+        return response.data[0];
+      } catch (err) {
+    console.error("Error fetching shipping cost:", err.message);
+    return null;
+  }
 };
 
 export default shippingCost;
